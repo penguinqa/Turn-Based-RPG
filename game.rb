8c925e3cbce032fe1cb5a76d1aec34c4eac85cf4
Donald McKendrick <ddmckendrick@gmail.com>
2013-04-12 01:05:09 +0200

    Adds attacks from enemy plus win/lose mechanic

--- game.rb
+++ game.rb
@@ -8,7 +8,15 @@ def attack(strength)
   return strength + rand(strength/10 + 2) - strength/10
 end
 
-until enemy_hp <= 0
+until enemy_hp <= 0 or hero_hp <= 0
   enemy_hp -= attack(hero_strength)
   puts enemy_hp
+  hero_hp -= attack(enemy_strength)
+  puts hero_hp
+end
+
+if enemy_hp <= 0
+  puts "You won the battle!"
+else
+  puts "You lost, hit the gym!"
 end
