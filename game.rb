8b104a00c597426d99365e90690e4d2699440407
Donald McKendrick <ddmckendrick@gmail.com>
2013-04-12 05:07:23 +0200

    Adds Gauntlet mode, Levelling and more
    Kind of playable in its current form. Running the code puts you into a gauntlet where you fight until you die Berserker style. Completely unbalanced.

--- game.rb
+++ game.rb
@@ -1,6 +1,8 @@
 hero = {
   name: "Donald",
   level: 1,
+  exp: 0,
+  max_hp: 100,
   hp: 100,
   strength: 10,
   wisdom: 5,
@@ -12,8 +14,9 @@ hero = {
 enemy = {
   name: "Gurlock",
   level: 1,
-  hp: 100,
-  strength: 10,
+  max_hp: 30,
+  hp: 30,
+  strength: 6,
   wisdom: 5,
   defense: 1,
   speed: 2,
@@ -26,8 +29,10 @@ inventory = {
     x_potion: 1    
   }
 
-def attack(strength)
-  return strength + (rand(strength) - strength/2) / 10
+def attack(attacker, victim)
+  damage = attacker[:strength] + (rand(attacker[:strength]) - attacker[:strength]/2) / 10
+  victim[:hp] -= damage
+  puts "#{attacker[:name]} strikes #{victim[:name]} for #{damage} points! #{victim[:name]} HP:#{victim[:hp]}"
 end
 
 def run(player, enemy)
@@ -47,29 +52,102 @@ def useItem(inventory)
   puts "Type the item you wish to use:"
   choice = gets.chomp.to_sym
   inventory[choice] -= 1
-end  
+end
+
+def reviveEnemy(enemy)
+  enemy[:hp] = enemy[:max_hp]
+end
+
+def calcExp(player, enemy)
+  if ( enemy[:level] - player[:level] ) < 1
+    bonus = enemy[:level] - player[:level]
+  else
+    bonus = 1
+  end
+  player[:exp] += enemy[:max_hp] * enemy[:strength] * bonus
+end
+
+def playerLvlUp(player)
+  levels = {
+    1 => 500,
+    2 => 1000,
+    3 => 2000,
+    4 => 4000,
+    5 => 7000,
+    6 => 9000,
+    7 => 14000,
+    8 => 20000,
+    9 => 30000,
+  }
+  if player[:exp] > levels[player[:level]]
+    puts "LEVEL UP!"
+    player[:level] += 1
+    stats = [:max_hp,:strength,:wisdom,:defense,:speed,:luck]
+    stats.each do |attribute|
+      if player[attribute]/10 < 1
+        player[attribute] += 1
+      else
+        player[attribute] += player[attribute]/10
+      end
+    end
+    displayStats(player)
+    player[:hp] = player[:max_hp]
+  else
+    puts "Next level in #{levels[player[:level]]- player[:exp]} points."
+  end
+end   
+
+def levelUpEnemy(enemy)
+  enemy[:level] += 1
+  stats = [:max_hp,:strength,:wisdom,:defense,:speed,:luck]
+  stats.each do |attribute|
+    if enemy[attribute]/10 < 1
+      enemy[attribute] += 1
+    else
+      enemy[attribute] += enemy[attribute]/10
+    end
+  end
+end
+
+def displayStats(enemy)
+  enemy.each do |attribute, stat|
+    puts "#{attribute.to_s.capitalize}: #{enemy[attribute]}"
+  end
+end    
 
 def battle(player, enemy)
   if player[:speed] >= enemy[:speed]
     until enemy[:hp] <= 0 or player[:hp] <= 0
-      enemy[:hp] -= attack(player[:strength])
-      puts "#{player[:name]} strikes #{enemy[:name]}! #{enemy[:name]} HP:#{enemy[:hp]}"
-      player[:hp] -= attack(enemy[:strength])
-      puts "#{enemy[:name]} strikes #{player[:name]}! #{player[:name]} HP:#{player[:hp]}"
+      attack(player, enemy)
+      attack(enemy, player)
     end
   else
     until enemy[:hp] <= 0 or player[:hp] <= 0
-      player[:hp] -= attack(enemy[:strength])
-      puts "#{enemy[:name]} strikes #{player[:name]}! #{player[:name]} HP:#{player[:hp]}"
-      enemy[:hp] -= attack(player[:strength])
-      puts "#{player[:name]} strikes #{enemy[:name]}! #{enemy[:name]} HP:#{enemy[:hp]}"
+      attack(enemy, player)
+      attack(player, enemy)
     end
   end
   if enemy[:hp] <= 0
     puts "You won the battle!"
+    levelUpEnemy(enemy)
+    reviveEnemy(enemy)
+    calcExp(player, enemy)
+    playerLvlUp(player)
   else
     puts "You lost, hit the gym!"
   end  
 end
 
-battle(hero, enemy)
+def gauntlet(player, enemy)
+  wins = 0
+  until player[:hp] <= 0
+    battle(player, enemy)
+    displayStats(enemy)
+    wins += 1
+    puts "Hit enter to fight the next battle."
+    gets.chomp
+  end
+  puts wins
+end 
+
+gauntlet(hero, enemy)
