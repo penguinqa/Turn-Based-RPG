80820d8dd018c3a670b724c93a25411f9548b056
Donald McKendrick <ddmckendrick@gmail.com>
2013-04-14 11:15:27 +0200

    Provides compatibility with new functions

--- game.rb
+++ game.rb
@@ -1,35 +1,38 @@
 require './functions.rb'
 
 hero = {
-  name: "Quinn",
+  name: "Quint",
   level: 1,
   exp: 0,
   max_hp: 100,
   hp: 100,
-  strength: 10,
+  max_mp: 24,
+  mp: 24,
+  strength: 12,
   wisdom: 5,
-  defense: 1,
+  base_defense: 5,
+  defense: 5,
   speed: 2,
   luck: 0,
   gender: "her"
 }
 
 enemy = {
-  name: "Josie",
+  name: "Rosie",
   level: 1,
-  max_hp: 30,
-  hp: 30,
-  strength: 6,
+  max_hp: 40,
+  hp: 40,
+  strength: 13,
   wisdom: 5,
-  defense: 1,
+  defense: 4,
   speed: 2,
   luck: 0
 }
 
 inventory = {
-    potion: 3,
-    hi_potion: 1,
-    x_potion: 1    
-  }
+  potion: 3,
+  hi_potion: 1,
+  x_potion: 1
+}
 
 gauntlet(hero, enemy, inventory)
